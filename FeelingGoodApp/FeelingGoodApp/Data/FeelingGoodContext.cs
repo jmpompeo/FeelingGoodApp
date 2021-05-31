b7bfd14002d20131776c68d89623535c47d1efc0
jmpompeo <joeypompeo@gmail.com>
2021-05-30 20:05:52 -0400

    created DevBranch

---
+++ FeelingGoodApp/FeelingGoodApp/Data/FeelingGoodContext.cs
@@ -0,0 +1,20 @@
+﻿using FeelingGoodApp.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FeelingGoodApp.Data
+{
+    public class FeelingGoodContext : IdentityDbContext
+    {
+        public FeelingGoodContext(DbContextOptions<FeelingGoodContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> EndUser { get; set; }
+        public DbSet<Exercises> Exercises { get; set; }
+    }
+}
---
+++ FeelingGoodApp/FeelingGoodApp/Data/Models/Exercise.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FeelingGoodApp.Data.Models
+{
+    public enum Exercise
+    { 
+        Swim,
+
+        Run,
+
+        Walk, 
+
+        Jog,
+
+        Bike,
+
+        Yoga,
+
+        Strength
+
+    }
+
+    public partial class Exercises
+    {
+        public Exercise Exercise { get; set; }
+    }
+}
