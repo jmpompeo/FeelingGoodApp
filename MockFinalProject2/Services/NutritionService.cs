a28716f6d6c47c575477d24821cdf3856d0d0b16
kmarion76 <47949532+kmarion76@users.noreply.github.com>
2021-06-02 22:22:25 -0400

    Initial Push

---
+++ MockFinalProject2/Services/NutritionService.cs
@@ -0,0 +1,28 @@
+﻿using MockFinalProject2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MockFinalProject2.Services
+{
+    public class NutritionService : INutritionService
+    {
+        private readonly HttpClient _client;
+        public NutritionService(HttpClient client)
+        {
+            _client = client;
+        }
+
+        [HttpPost]
+        public async Task<UserNutrition> GetName()
+        {
+            
+            var meal =  await _client.GetFromJsonAsync<UserNutrition>("/v2/natural/nutrients");
+            return meal;
+        }
+    }
+}
