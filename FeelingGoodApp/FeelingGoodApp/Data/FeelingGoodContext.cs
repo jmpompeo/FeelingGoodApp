f0bda2947cfd4899862cc144b704fdf286204709
jmpompeo <joeypompeo@gmail.com>
2021-06-17 21:38:43 -0400

    css

--- FeelingGoodApp/FeelingGoodApp/Data/FeelingGoodContext.cs
+++ FeelingGoodApp/FeelingGoodApp/Data/FeelingGoodContext.cs
@@ -14,15 +14,13 @@ namespace FeelingGoodApp.Services.Models
         }
 
         //public DbSet<ExerciseRequest> EndUser { get; set; }
-        public DbSet<Exercises> Exercises { get; set; }
+        //public DbSet<Exercises> Exercises { get; set; }
         public DbSet<ExerciseInfo> ExerciseInfo { get; set; }
-        public DbSet<ExerciseResponse> ExerciseResponse { get; set; }
-        public DbSet<LocationViewModel> LocationViewModel { get; set; }
-        public DbSet<Location> Location { get; set; }
+        //public DbSet<ExerciseResponse> ExerciseResponse { get; set; }
         public DbSet<ApplicationUser> EndUser { get; set; }
 
         public DbSet<NutritionViewModel> MealData { get; set; }
 
-        public DbSet<IndexViewModel> IndexViewModel { get; set; }
+        //public DbSet<IndexViewModel> IndexViewModel { get; set; }
     }
 }
